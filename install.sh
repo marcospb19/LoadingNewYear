a59aa27ff04d1e1f26c036e79ba8cf6e3569bdef
João Marcos <marcospb19@hotmail.com>
2019-04-15 19:20:01 -0300

    Better documenting.

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "\e[36;1m" && \time -f " Time: %es" gcc -Werror -Wall source/terminal.c main.c -o loadingnewyear -lm && printf " Build successful\n"
+printf "\e[36;1m" && gcc -Werror -Wall source/terminal.c main.c -o loadingnewyear -lm && printf " Build successful\n"
--- source/terminal.c
+++ source/terminal.c
@@ -1,6 +1,7 @@
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <time.h>
+#include <sys/ioctl.h> /* getWindowSize() */
+#include <unistd.h>    /* getWindowSize() */
+#include <time.h>      /* getDayOfYear ()  */
+
 
 // Return the number of columns of the terminal
 int getWindowSize()
@@ -8,8 +9,18 @@ int getWindowSize()
 	struct winsize terminalWindow;
 	ioctl(STDOUT_FILENO , TIOCGWINSZ , &terminalWindow);
 	return terminalWindow.ws_col;
-	// *lines = terminalWindow.ws_row;
+
+	// From winsize struct:
+
+	// struct winsize
+	// {
+	// 	unsigned short  ws_row;         /* rows, in characters */
+	// 	unsigned short  ws_col;         /* columns, in characters */
+	// 	unsigned short  ws_xpixel;      /* horizontal size, pixels - not used */
+	// 	unsigned short  ws_ypixel;      /* vertical size, pixels - not used */
+	// };
 }
+
 
 // Return the number of day of the year
 int getDayOfYear()
@@ -18,4 +29,19 @@ int getDayOfYear()
 	struct tm* timeinfo;
 	timeinfo = localtime(&rawTime); // Fill the struct with actual time
 	return timeinfo->tm_yday; // Return only the dayOfYear
+
+	// From tm struct:
+
+	// struct tm
+	// {
+	// 	int tm_sec;         /* seconds,  range 0 to 59          */
+	// 	int tm_min;         /* minutes, range 0 to 59           */
+	// 	int tm_hour;        /* hours, range 0 to 23             */
+	// 	int tm_mday;        /* day of the month, range 1 to 31  */
+	// 	int tm_mon;         /* month, range 0 to 11             */
+	// 	int tm_year;        /* The number of years since 1900   */
+	// 	int tm_wday;        /* day of the week, range 0 to 6    */
+	// 	int tm_yday;        /* day in the year, range 0 to 365  */
+	// 	int tm_isdst;       /* daylight saving time             */
+	// };
 }
